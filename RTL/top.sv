6cc9941e12902aa762083de9de75d5e1802e404b
Clement Hathaway <cwbh10@gmail.com>
2019-02-28 09:40:26 -0500

    added offset counter to top and connected it appropriately to the fsm

--- RTL/top.sv
+++ RTL/top.sv
@@ -22,12 +22,17 @@ module top(input logic clk, reset_button,
 	logic [4:0] col_count;
 	col_counter col_counter(.clk(clk), .reset(reset_debounced), .enable(col_counter_enable), .row_filled(row_filled), .count(col_count));
 	
+	// Setup the offset counter module
+	logic offset_counter_enable;
+	logic [9:0] offset_count;
+	offset_counter offset_counter(.clk(clk), .reset(reset_debounced), .enable(offset_counter_enable), .count(offset_count));
+	
 	// Setup the pixel-gen module
 	pixel_generator pixel_generator(.clk(clk), .reset(reset_debounced), .col_count(col_count), .row_count(row_count), .LED_Top(LED_Top), .LED_Bottom(LED_Bottom));
 	
 	// Setup the main FSM
-	fsm main_fsm(.clk(clk), .reset(reset_debounced), .delay_enb(delay_enable), .col_count(col_count), .row_filled(row_filled),
-	             .col_counter_enb(col_counter_enable), .row_counter_enb(row_counter_enable),
+	fsm main_fsm(.clk(clk), .reset(reset_debounced), .delay_enb(delay_enable), .col_count(col_count), .row_count(row_count), .row_filled(row_filled),
+	             .col_counter_enb(col_counter_enable), .row_counter_enb(row_counter_enable), .offset_counter_enable(offset_counter_enable),
 				 .sclk(sclk), .blank(blank), .latch(latch));
 	
 endmodule

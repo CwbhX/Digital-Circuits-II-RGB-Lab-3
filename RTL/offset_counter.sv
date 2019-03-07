fc4245d91094856c7dfa63ed4428086e0ef14abe
Clement Hathaway <cwbh10@gmail.com>
2019-03-07 09:11:33 -0500

    Updated offset counter to count number of frames before actually incrementing

--- RTL/offset_counter.sv
+++ RTL/offset_counter.sv
@@ -23,7 +23,17 @@
 module offset_counter(input logic clk, reset, enable,
 				      output logic [9:0] count);
 	
-	binaryCounter #(.MAXVAL(1024)) counter_10bit(.clk(clk), .reset(reset), .enable(enable), .out(count));
+	// Allows us to choose the number of frames we want to wait from the higher level module
+	parameter FRAME_COUNT = 100;
+	logic [$clog2(FRAME_COUNT):0] frame_count;
+	logic offset_count_enable;
 	
+	binaryCounter #(.MAXVAL(FRAME_COUNT)) counter_frames(.clk(clk), .reset(reset), .enable(enable), .out(frame_count)); // Count frames
 	
+	binaryCounter #(.MAXVAL(1024)) counter_10bit(.clk(clk), .reset(reset), .enable(offset_count_enable), .out(count)); // Count for the memory offset
+	
+	always_comb begin // If we have reached x number of frames, updated the offset by 1
+		if(frame_count == FRAME_COUNT) offset_count_enable = 1;
+		else offset_count_enable = 0;
+	end
 endmodule
